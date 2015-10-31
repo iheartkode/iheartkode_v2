ec51f79d294221eb5709ed7d5b029431d1cbc078
iheartkode <y2kmark73@gmail.com>
2015-10-30 22:09:55 -0700

    added more styling to the menu with jquery

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mark on 10/30/2015.
+ */
+$(document).ready(function () {
+    $("#home").on("mouseover",function() {
+        $("#home").addClass("hover-nav");
+    });
+    $("#home").on("mouseout",function() {
+        $("#home").removeClass("hover-nav");
+    });
+    $("#about").on("mouseover",function() {
+        $("#about").addClass("hover-nav");
+    });
+    $("#about").on("mouseout",function() {
+        $("#about").removeClass("hover-nav");
+    });
+    $("#projects").on("mouseover",function() {
+        $("#projects").addClass("hover-nav");
+    });
+    $("#projects").on("mouseout",function() {
+        $("#projects").removeClass("hover-nav");
+    });
+
+});
